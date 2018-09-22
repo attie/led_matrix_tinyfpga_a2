2d5130aea013b1c2192c03d921eacd46ff55c66f
Attie Grande <attie@attie.co.uk>
2018-09-22 22:44:23 +0100

    updated the uart clock divider

--- control_module.v
+++ control_module.v
@@ -1,7 +1,7 @@
 module control_module #(
 	/* UART configuration */
-	parameter UART_CLK_DIV_COUNT = 30, /* 7 MHz in / 30 = ~115,200 baud (actually ~116,686 baud, +1.29%) */
-	parameter UART_CLK_DIV_WIDTH = 5
+	parameter UART_CLK_DIV_COUNT = 231, /* 53.2 MHz in / 231 = ~115,200 baud (actually ~115,151 baud, -0.04%) */
+	parameter UART_CLK_DIV_WIDTH = 8
 ) (
 	input reset,
 	input clk_in,
