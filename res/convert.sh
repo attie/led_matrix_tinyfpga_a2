1bd1733bcabbfc36180cb8d7e073b19f8dc53b2d
Attie Grande <attie@attie.co.uk>
2018-09-22 15:45:21 +0100

    added some resources

---
+++ res/convert.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -eu
+
+usage() {
+	{
+		while [ $# -gt 0 ]; do
+			echo "${1}"
+			shift
+		done
+		echo "usage:"
+		echo "    ${0} <input file>"
+	} >&2
+	exit 1
+}
+
+[ $# -ne 1 ] && usage
+
+INPUT="${1}"; shift
+
+[ ! -s "${INPUT}" ] && usage "${INPUT}: file not found"
+
+OUTPUT_BASE="${INPUT%%.*}"
+
+# convert input file to a raw framebuffer
+gst-launch-1.0 -v \
+	filesrc "location=${INPUT}" \
+	! decodebin \
+	! videoconvert \
+	! video/x-raw,format=RGB16,width=64,height=32,framerate=0/1 \
+	! filesink "location=${OUTPUT_BASE}.bin"
+
+# convert the raw frame buffer to one hex byte per-line for IPexpress
+xxd -p -c1 \
+	< "${OUTPUT_BASE}.bin" \
+	| sed -re 's/^(..)(..)$/\2\n\1/' \
+	> "${OUTPUT_BASE}.mem"
