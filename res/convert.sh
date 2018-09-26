f54cb03df8a930fec1b3d1209765715671ab1b1a
Attie Grande <attie@attie.co.uk>
2018-09-26 22:55:13 +0100

    added place-holder conversion to RGB888

--- res/convert.sh
+++ res/convert.sh
@@ -24,6 +24,8 @@ OUTPUT_DIR="${INPUT%/*}"
 OUTPUT_NAME="${INPUT##*/}"
 OUTPUT_BASE="${OUTPUT_NAME%.*}"
 
+## 16-bit / RGB565
+
 # convert input file to a raw framebuffer
 gst-launch-1.0 -v \
 	filesrc "location=${INPUT}" \
@@ -45,3 +47,29 @@ xxd -p -c$((64 * 2)) \
 	| awk '{printf "4C%02x%s\n", NR-1, $0}' \
 	| xxd -r -p \
 	> "${OUTPUT_DIR}/${OUTPUT_BASE}.uart"
+
+: <<"EOF"
+## 24-bit / RGB888 - TODO
+
+# convert input file to a raw framebuffer
+gst-launch-1.0 -v \
+	filesrc "location=${INPUT}" \
+	! decodebin \
+	! gamma gamma=0.4 \
+	! videoconvert dither=GST_VIDEO_DITHER_NONE \
+	! video/x-raw,format=RGB,width=64,height=32,framerate=0/1 \
+	! filesink "location=${OUTPUT_DIR}/.${OUTPUT_BASE}.bin"
+
+# convert the raw frame buffer to one hex byte per-line for IPexpress
+xxd -p -c1 \
+	< "${OUTPUT_DIR}/.${OUTPUT_BASE}.bin" \
+	> "${OUTPUT_DIR}/${OUTPUT_BASE}.mem"
+
+# convert the raw frame buffer to lines to be sent via UART
+xxd -p -c$((64 * 3)) \
+	< "${OUTPUT_DIR}/.${OUTPUT_BASE}.bin" \
+	| sed -re 's/(..)(..)(..)/\3\2\1/g' \
+	| awk '{printf "4C%02x%s\n", NR-1, $0}' \
+	| xxd -r -p \
+	> "${OUTPUT_DIR}/${OUTPUT_BASE}.uart"
+EOF
